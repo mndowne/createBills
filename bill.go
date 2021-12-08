260a400bb003f4c64b2542d5e76a8099dac65ea4
Martin Downes <mndowne@gmail.com>
2021-12-08 10:20:34 -0600

    able to read user input and create bills

--- bill.go
+++ bill.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type bill struct {
 	name  string
@@ -31,8 +34,33 @@ func (b bill) format() string {
 		total += v
 	}
 
+	//tip
+	fs += fmt.Sprintf("%-25v ...$%0.2f \n", "tip:", b.tip)
+
 	//total
-	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
 
 	return fs
+}
+
+//update tip
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+}
+
+// add an item to the bill
+func (b bill) addItem(name string, price float64) {
+	b.items[name] = price
+}
+
+// save bill
+func (b *bill) save() {
+	data := []byte(b.format())
+
+	err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("bill was saved to file")
+
 }
